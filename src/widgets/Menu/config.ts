458b8b8f115aca3f99507dd86df73bf35f8a9056
Adventureland DeFi <mail@adventureland.finance>
2021-03-30 00:16:51 -0600

    feat: Setting twitter address

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -114,70 +114,70 @@ export const links = [
 ];
 
 export const socials = [
-  {
-    label: "Telegram",
-    icon: "TelegramIcon",
-    items: [
-      {
-        label: "English",
-        href: "https://t.me/goosedefi",
-      },
-      // {
-      //   label: "Bahasa Indonesia",
-      //   href: "https://t.me/PancakeSwapIndonesia",
-      // },
-      // {
-      //   label: "中文",
-      //   href: "https://t.me/PancakeSwap_CN",
-      // },
-      // {
-      //   label: "Tiếng Việt",
-      //   href: "https://t.me/PancakeSwapVN",
-      // },
-      // {
-      //   label: "Italiano",
-      //   href: "https://t.me/pancakeswap_ita",
-      // },
-      // {
-      //   label: "русский",
-      //   href: "https://t.me/pancakeswap_ru",
-      // },
-      // {
-      //   label: "Türkiye",
-      //   href: "https://t.me/pancakeswapturkiye",
-      // },
-      // {
-      //   label: "Português",
-      //   href: "https://t.me/PancakeSwapPortuguese",
-      // },
-      // {
-      //   label: "Español",
-      //   href: "https://t.me/PancakeswapEs",
-      // },
-      // {
-      //   label: "日本語",
-      //   href: "https://t.me/pancakeswapjp",
-      // },
-      // {
-      //   label: "Français",
-      //   href: "https://t.me/pancakeswapfr",
-      // },
-      {
-        label: "Announcements",
-        href: "https://t.me/goosefinanceann",
-      },
-      // {
-      //   label: "Whale Alert",
-      //   href: "https://t.me/PancakeSwapWhales",
-      // },
-    ],
-  },
+  // {
+  //   label: "Telegram",
+  //   icon: "TelegramIcon",
+  //   items: [
+  //     {
+  //       label: "English",
+  //       href: "https://t.me/goosedefi",
+  //     },
+  //     // {
+  //     //   label: "Bahasa Indonesia",
+  //     //   href: "https://t.me/PancakeSwapIndonesia",
+  //     // },
+  //     // {
+  //     //   label: "中文",
+  //     //   href: "https://t.me/PancakeSwap_CN",
+  //     // },
+  //     // {
+  //     //   label: "Tiếng Việt",
+  //     //   href: "https://t.me/PancakeSwapVN",
+  //     // },
+  //     // {
+  //     //   label: "Italiano",
+  //     //   href: "https://t.me/pancakeswap_ita",
+  //     // },
+  //     // {
+  //     //   label: "русский",
+  //     //   href: "https://t.me/pancakeswap_ru",
+  //     // },
+  //     // {
+  //     //   label: "Türkiye",
+  //     //   href: "https://t.me/pancakeswapturkiye",
+  //     // },
+  //     // {
+  //     //   label: "Português",
+  //     //   href: "https://t.me/PancakeSwapPortuguese",
+  //     // },
+  //     // {
+  //     //   label: "Español",
+  //     //   href: "https://t.me/PancakeswapEs",
+  //     // },
+  //     // {
+  //     //   label: "日本語",
+  //     //   href: "https://t.me/pancakeswapjp",
+  //     // },
+  //     // {
+  //     //   label: "Français",
+  //     //   href: "https://t.me/pancakeswapfr",
+  //     // },
+  //     {
+  //       label: "Announcements",
+  //       href: "https://t.me/goosefinanceann",
+  //     },
+  //     // {
+  //     //   label: "Whale Alert",
+  //     //   href: "https://t.me/PancakeSwapWhales",
+  //     // },
+  //   ],
+  // },
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/FinanceGoose",
+    href: "https://twitter.com/AdventurelandF1",
   },
-];
+] as any[];
 
 export const MENU_HEIGHT = 64;
 export const MENU_ENTRY_HEIGHT = 48;
